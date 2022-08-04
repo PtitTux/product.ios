5567ea05c66b26612be4cf570c069407640eac23
Arnaud JEANNE <arnaud.jeanne@gmail.com>
2022-08-04 13:40:03 +0200

    test: Add userEntity Test

--- src/test/java/io/product/server/repositories/UserRepositoryTest.java
+++ src/test/java/io/product/server/repositories/UserRepositoryTest.java
@@ -4,12 +4,14 @@ import io.product.server.entities.UserEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.ActiveProfiles;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@DataJpaTest
 @ActiveProfiles("test")
+@DataJpaTest
 class UserRepositoryTest
 {
 	@Autowired
@@ -25,5 +27,7 @@ class UserRepositoryTest
 		user = userRepository.save(user);
 
 		assertThat(user.getId()).isNotNull();
+		assertThat(user.getName()).isEqualTo("name");
+		assertThat(user.getEmail()).isEqualTo("email");
 	}
 }
