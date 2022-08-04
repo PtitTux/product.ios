f246a014b0eff180ccda2d90e054eeb26ddda450
Arnaud JEANNE <arnaud.jeanne@gmail.com>
2022-08-04 17:16:23 +0200

    feat: active test profile

--- src/test/java/io/product/server/controllers/UserControllerTest.java
+++ src/test/java/io/product/server/controllers/UserControllerTest.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class UserControllerTest
