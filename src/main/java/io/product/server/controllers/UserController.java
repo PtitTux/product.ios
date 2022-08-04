a71f0b77c3475ab1dc0d7e5ef018b6d9d45f69eb
Arnaud JEANNE <arnaud.jeanne@gmail.com>
2022-08-04 17:13:00 +0200

    feat: Add UserController

---
+++ src/main/java/io/product/server/controllers/UserController.java
@@ -0,0 +1,36 @@
+package io.product.server.controllers;
+
+import io.product.server.dto.UserDTO;
+import io.product.server.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController
+{
+	@Autowired
+	UserRepository repository;
+
+	@Autowired
+	ModelMapper modelMapper;
+
+	@GetMapping("/")
+	public ResponseEntity<Object> findAll() {
+		List<UserDTO> users =  repository.findAll().stream().map(u -> modelMapper.map(u, UserDTO.class)).collect(Collectors.toList());
+
+		return new APIResponse("Successfully retrieved users")
+		        .setData(users)
+		        .build();
+
+	}
+}
---
+++ src/test/java/io/product/server/controllers/UserControllerTest.java
@@ -0,0 +1,28 @@
+package io.product.server.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UserControllerTest
+{
+	@Autowired
+	UserController userController;
+
+
+	@Test
+	public void testFindAll() {
+		ResponseEntity<Object> response = userController.findAll();
+
+		assertThat(response).isNotNull();
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+}
