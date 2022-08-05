dfd64a714d99624fab24ce7fba275fffe5e899d3
Arnaud JEANNE <arnaud.jeanne@gmail.com>
2022-08-05 09:51:38 +0200

    refactor: Remove unused import

--- src/main/java/io/product/server/controllers/UserController.java
+++ src/main/java/io/product/server/controllers/UserController.java
@@ -1,17 +1,12 @@
 package io.product.server.controllers;
 
-import io.product.server.dto.UserDTO;
-import io.product.server.repositories.UserRepository;
 import io.product.server.services.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
