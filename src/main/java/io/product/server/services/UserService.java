8caf3924d2256f33ca0ed9b7971660ed35ae89d6
Arnaud JEANNE <arnaud.jeanne@gmail.com>
2022-08-05 09:52:19 +0200

    refactor: Add debug log

--- src/main/java/io/product/server/services/UserService.java
+++ src/main/java/io/product/server/services/UserService.java
@@ -2,6 +2,7 @@ package io.product.server.services;
 
 import io.product.server.dto.UserDTO;
 import io.product.server.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Slf4j
 public class UserService
 {
 	@Autowired
@@ -18,6 +20,7 @@ public class UserService
 	ModelMapper modelMapper;
 
 	public List<UserDTO> findAll() {
+		log.debug("Find All users");
 		return repository.findAll().stream().map(u -> modelMapper.map(u, UserDTO.class)).toList();
 	}
 }
