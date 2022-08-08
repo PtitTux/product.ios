feafa8caa43575e1b23552a68e230da8187a5b03
Arnaud JEANNE <arnaud.jeanne@gmail.com>
2022-08-08 08:20:33 +0200

    feat: Add AuthController

---
+++ src/main/java/io/product/server/controllers/AuthController.java
@@ -0,0 +1,10 @@
+package io.product.server.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController
+{
+}
