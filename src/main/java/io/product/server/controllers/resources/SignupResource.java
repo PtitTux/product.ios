32389f46d6d129e693b95da4e44a977cdea228c1
Arnaud JEANNE <arnaud.jeanne@gmail.com>
2022-08-09 14:59:53 +0200

    test: Add AuthControllerTest

--- src/main/java/io/product/server/controllers/resources/SignupResource.java
+++ src/main/java/io/product/server/controllers/resources/SignupResource.java
@@ -1,11 +1,13 @@
 package io.product.server.controllers.resources;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 @Data
+@Builder
 public class SignupResource
 {
 	@NotNull
---
+++ src/test/java/io/product/server/controllers/AuthControllerTest.java
@@ -0,0 +1,66 @@
+package io.product.server.controllers;
+
+import io.product.server.controllers.resources.SignupResource;
+import io.product.server.exceptions.UserExistException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class AuthControllerTest
+{
+	@Autowired
+	AuthController authController;
+
+	@Test
+	void testSignupValid()
+	{
+		SignupResource signup = SignupResource.builder().email("test@product.io").name("Test").password("Password").build();
+
+		try
+		{
+			ResponseEntity<Object> response = authController.signup(signup);
+
+			assertThat(response).isNotNull();
+			assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+			assertThat(response.hasBody()).isTrue();
+		}
+		catch (UserExistException e)
+		{
+			assertThat(e).doesNotThrowAnyException();
+		}
+	}
+
+	@Test
+	void testSignupWithoutEmail()
+	{
+		SignupResource signup = SignupResource.builder().name("Test").password("Password").build();
+
+		assertThatThrownBy(() -> authController.signup(signup)).isInstanceOf(Exception.class);
+	}
+
+	@Test
+	void testSignupWithoutName()
+	{
+		SignupResource signup = SignupResource.builder().email("test@product.io").password("Password").build();
+
+		assertThatThrownBy(() -> authController.signup(signup)).isInstanceOf(Exception.class);
+	}
+	@Test
+	void testSignupWithoutPassword()
+	{
+		SignupResource signup = SignupResource.builder().name("Test").email("test@product.io").build();
+
+		assertThatThrownBy(() -> authController.signup(signup)).isInstanceOf(Exception.class);
+	}
+}
