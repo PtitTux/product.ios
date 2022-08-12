134cd2067daf1484ee525a516019c6207c7e9fa4
Arnaud JEANNE <arnaud.jeanne@gmail.com>
2022-08-12 08:53:54 +0200

    test: Add JWT utils

---
+++ src/main/java/io/product/server/security/JWTUtils.java
@@ -0,0 +1,44 @@
+package io.product.server.security;
+
+import io.jsonwebtoken.*;
+import io.product.server.entities.UserEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Component
+public class JWTUtils
+{
+	@Value("${product.io.jwtSecret}")
+	private String jwtSecret;
+
+	@Value("${product.io.jwtExpirationMs}")
+	private int jwtExpirationMs;
+
+	public String generateToken(UserEntity userEntity)
+	{
+		return Jwts.builder()
+		           .setIssuer("product.io")
+		           .setSubject((userEntity.getEmail()))
+		           .setIssuedAt(new Date())
+		           .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+		           .signWith(SignatureAlgorithm.HS512, jwtSecret)
+		           .compact();
+	}
+
+	public Optional<String> validateToken(String token)
+	{
+		try
+		{
+			Jwt jwt = Jwts.parser().setSigningKey(jwtSecret).parse(token);
+			Claims claims = (Claims) jwt.getBody();
+			return Optional.of(claims.getSubject());
+		}
+		catch (ExpiredJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e)
+		{
+			return Optional.empty();
+		}
+	}
+}
---
+++ src/test/java/io/product/server/security/JWTUtilsTest.java
@@ -0,0 +1,49 @@
+package io.product.server.security;
+
+import io.product.server.entities.UserEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class JWTUtilsTest
+{
+	@Mock
+	JWTUtils utils;
+
+	UserEntity userTest;
+
+	@BeforeEach
+	void setUp()
+	{
+		utils = new JWTUtils();
+		ReflectionTestUtils.setField(utils, "jwtSecret", "MyJwTSecret2022");
+		ReflectionTestUtils.setField(utils, "jwtExpirationMs", 86400000);
+
+		userTest = UserEntity.builder().name("name").email("email").status(true).password("password").build();
+	}
+
+	@Test
+	void generateToken()
+	{
+		String token = utils.generateToken(userTest);
+		assertThat(token).isNotNull();
+	}
+
+	@Test
+	void validateToken() {
+		String token = utils.generateToken(userTest);
+
+		Optional<String> body = utils.validateToken(token);
+		assertThat(body).isPresent();
+		assertThat(body).contains("email");
+	}
+}
