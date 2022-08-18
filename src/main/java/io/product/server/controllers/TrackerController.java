e1df19b7a2e6d721a9a044680726b9f3a7440de1
Arnaud JEANNE <arnaud.jeanne@gmail.com>
2022-08-18 14:23:35 +0200

    feat: Add method api controller to create tracker

--- src/main/java/io/product/server/controllers/TrackerController.java
+++ src/main/java/io/product/server/controllers/TrackerController.java
@@ -1,13 +1,18 @@
 package io.product.server.controllers;
 
+import io.product.server.controllers.resources.CreateTrackerResource;
 import io.product.server.controllers.resources.TrackerListResource;
+import io.product.server.controllers.resources.TrackerResource;
+import io.product.server.dto.Tracker;
+import io.product.server.exceptions.TrackerExistException;
 import io.product.server.services.TrackerService;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/trackers")
@@ -27,8 +32,16 @@ public class TrackerController
 	public ResponseEntity<Object> findAll()
 	{
 		return new APIResponse<>("Successfully retrieved trackers").setData(service.findAll()
-		                                                                        .stream()
-		                                                                        .map(u -> this.modelMapper.map(u, TrackerListResource.class))
-		                                                                        .toList()).build();
+		                                                                           .stream()
+		                                                                           .map(u -> this.modelMapper.map(u, TrackerListResource.class))
+		                                                                           .toList()).build();
+	}
+
+	@PostMapping("")
+	public ResponseEntity<Object> createTracker(@Valid @RequestBody CreateTrackerResource t) throws TrackerExistException
+	{
+		Tracker tracker = this.service.createTracker(t.getName(), t.getDescription(), t.isDefaultTracker());
+
+		return new APIResponse<>("tracker signup with success").setStatus(HttpStatus.CREATED).setData(this.modelMapper.map(tracker, TrackerResource.class)).build();
 	}
 }
