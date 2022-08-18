d4f3d5ef6e72c112b4af0cb23fc1406684bc61d4
Arnaud JEANNE <arnaud.jeanne@gmail.com>
2022-08-18 08:23:10 +0200

    test: Adding testing JWT Filter

---
+++ src/test/java/io/product/server/security/jwt/JWTFilterTest.java
@@ -0,0 +1,95 @@
+package io.product.server.security.jwt;
+
+import io.product.server.entities.UserEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class JWTFilterTest
+{
+	@Autowired
+	private TestRestTemplate testClient;
+
+	private JWTUtils jwtUtils;
+
+	@BeforeEach
+	void setUp() {
+		jwtUtils = new JWTUtils();
+		ReflectionTestUtils.setField(jwtUtils, "jwtSecret", "MyJwTSecret2022");
+		ReflectionTestUtils.setField(jwtUtils, "jwtExpirationMs", 86400000);
+	}
+
+	@Test
+	void testPublicAPI() {
+		Map<String,String> signup=new HashMap<>();
+		signup.put("email","jane@product.io");
+		signup.put("name","Jane");
+		signup.put("password","P@ssw@rd");
+
+		ResponseEntity<String> response = testClient.postForEntity("/auth/signup",signup, String.class);
+		assertThat(response.getStatusCode().value()).isEqualTo(201);
+	}
+
+	@Test
+	void testPrivateAPIWithoutAuth() {
+		ResponseEntity<String> response = testClient.getForEntity("/users", String.class);
+		assertThat(response.getStatusCode().value()).isEqualTo(403);
+	}
+
+	@Test
+	void testPrivateAPIWithAuth() {
+		Map<String,String> signup=new HashMap<>();
+		signup.put("email","john@product.io");
+		signup.put("name","John");
+		signup.put("password","P@ssw@rd");
+
+		testClient.postForEntity("/auth/signup",signup, String.class);
+
+		UserEntity john = UserEntity.builder().name("John").email("john@product.io").password("P@ssw@rd").build();
+		String token = jwtUtils.generateToken(john);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", token);
+
+		RequestEntity requestEntity = new RequestEntity(headers, HttpMethod.GET, URI.create("/users"));
+
+		ResponseEntity<String> response = testClient.exchange(requestEntity, String.class);
+		assertThat(response.getStatusCode().value()).isEqualTo(200);
+	}
+
+	@Test
+	void testPrivateAPIWithAuthBearer() {
+		Map<String,String> signup=new HashMap<>();
+		signup.put("email","john2@product.io");
+		signup.put("name","John");
+		signup.put("password","P@ssw@rd");
+
+		testClient.postForEntity("/auth/signup",signup, String.class);
+
+		UserEntity john = UserEntity.builder().name("John").email("john2@product.io").password("P@ssw@rd").build();
+		String token = jwtUtils.generateToken(john);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer "+token);
+
+		RequestEntity requestEntity = new RequestEntity(headers, HttpMethod.GET, URI.create("/users"));
+
+		ResponseEntity<String> response = testClient.exchange(requestEntity, String.class);
+		assertThat(response.getStatusCode().value()).isEqualTo(200);
+	}
+}
