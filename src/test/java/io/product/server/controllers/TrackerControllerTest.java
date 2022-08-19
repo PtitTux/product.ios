267e697f57e883f123c676bc824f39ad73967e6e
Arnaud JEANNE <arnaud.jeanne@gmail.com>
2022-08-19 14:52:09 +0200

    test: add tracker tests controllers

--- src/test/java/io/product/server/controllers/TrackerControllerTest.java
+++ src/test/java/io/product/server/controllers/TrackerControllerTest.java
@@ -2,6 +2,7 @@ package io.product.server.controllers;
 
 import io.product.server.controllers.resources.CreateTrackerResource;
 import io.product.server.exceptions.TrackerExistException;
+import io.product.server.exceptions.TrackerNotExistException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -33,7 +37,8 @@ class TrackerControllerTest
 	}
 
 	@Test
-	void testCreateTracker() {
+	void testCreateTracker()
+	{
 		CreateTrackerResource t = CreateTrackerResource.builder().name("testing").defaultTracker(false).build();
 
 		try
@@ -50,9 +55,30 @@ class TrackerControllerTest
 	}
 
 	@Test
-	void testCreateTrackerWithoutName() {
+	void testCreateTrackerWithoutName()
+	{
 		CreateTrackerResource t = CreateTrackerResource.builder().defaultTracker(false).build();
 
 		assertThatThrownBy(() -> controller.createTracker(t)).isInstanceOf(Exception.class);
+	}
+
+	@Test
+	void testUpdateTracker()
+	{
+		try
+		{
+			Map<String, Object> body = new HashMap<>();
+			body.put("name", "testing-bug2");
+
+
+			ResponseEntity<Object> response = controller.updateTracker("0875ebf4-fff7-46b9-8694-b8e134421b09", body);
+			assertThat(response).isNotNull();
+			assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+			assertThat(response.hasBody()).isTrue();
+		}
+		catch (TrackerNotExistException e)
+		{
+			throw new RuntimeException(e);
+		}
 	}
 }
